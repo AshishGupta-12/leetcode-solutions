d4de5e832fbb61a155d5d57abdf9448f6efd51ed
AshishGupta-12 <ashishgupta11267@gmail.com>
2026-06-09 16:56:39 +0530

    Add solution for LeetCode 206 - Reverse Linked List
    Implemented iterative linked list reversal using three pointers (prev, curr, next).

---
+++ LeetCode/0206_Reverse_Linked_List.cpp
@@ -0,0 +1,43 @@
+/*
+LeetCode 206 - Reverse Linked List
+
+Approach:
+Use three pointers:
+1. prev -> previous node
+2. curr -> current node
+3. next -> stores next node temporarily
+
+Reverse each link one by one while traversing the list.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* next = NULL;
+
+        while(curr != NULL){
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
