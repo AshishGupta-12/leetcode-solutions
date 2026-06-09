e313dca823a664a6da0adb5e37250ac1c94bdca1
AshishGupta-12 <ashishgupta11267@gmail.com>
2026-06-09 16:57:45 +0530

    Add solution for LeetCode 876 - Middle of the Linked List
    Implemented the slow and fast pointer technique to find the middle node of a linked list.

---
+++ LeetCode/0876_Middle_of_the_Linked_List.cpp
@@ -0,0 +1,40 @@
+/*
+LeetCode 876 - Middle of the Linked List
+
+Approach:
+Use two pointers:
+1. Slow pointer moves one step at a time.
+2. Fast pointer moves two steps at a time.
+
+When the fast pointer reaches the end of the list,
+the slow pointer will be at the middle node.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        return slow;
+    }
+};
